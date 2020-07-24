735fb4bbe31859979c25d4ea006d442d1971d7c9
Christhonie Geldenhuys <development@christhonie.co.za>
2020-07-24 11:03:50 +0200

    Refactor into separate class

---
+++ src/main/java/za/co/idealogic/javaexercise/GreetingService.java
@@ -0,0 +1,69 @@
+package za.co.idealogic.javaexercise;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GreetingService {
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	public void process(String... args) {
+		final Options options = new Options();
+
+        Option countOption = Option.builder("c")
+        		.required(true)
+        		.longOpt( "count" )
+        		.hasArg()
+        		.argName("count")
+                .desc("How many times to print Hello World")
+                .build();
+        options.addOption(countOption);
+
+        Option byeOption = Option.builder("b")
+        		.required(false)
+                .desc("Say goodbye too")
+                .build();
+        options.addOption(byeOption);
+
+        final CommandLineParser parser = new DefaultParser();
+        final HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            log.error(e.getMessage(),e);
+            formatter.printHelp("Java Exercise 2", options);
+
+            System.exit(1);
+            return;
+        }
+
+        final boolean bye = cmd.hasOption('b');
+
+        if (cmd.hasOption('c')) {
+        	String countStr = cmd.getOptionValue("count");
+      		try {
+				Integer i = Integer.parseInt(countStr);
+				while (i-- > 0) {
+					log.info("Hello World");
+				}
+			} catch (NumberFormatException e) {
+				log.error("Invalid value for count variable");
+				System.exit(1);
+	            return;
+			}
+        }
+        
+        if(bye) {
+        	log.info("Bye world");
+        }
+	}
+}
--- src/main/java/za/co/idealogic/javaexercise/JavaExerciseApplication.java
+++ src/main/java/za/co/idealogic/javaexercise/JavaExerciseApplication.java
@@ -1,12 +1,5 @@
 package za.co.idealogic.javaexercise;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -26,56 +19,8 @@ public class JavaExerciseApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		log.info("Java Exercise 3: Application started!");
 		
-		final Options options = new Options();
-
-        Option countOption = Option.builder("c")
-        		.required(true)
-        		.longOpt( "count" )
-        		.hasArg()
-        		.argName("count")
-                .desc("How many times to print Hello World")
-                .build();
-        options.addOption(countOption);
-
-        Option byeOption = Option.builder("b")
-        		.required(false)
-                .desc("Say goodbye too")
-                .build();
-        options.addOption(byeOption);
-
-        final CommandLineParser parser = new DefaultParser();
-        final HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            log.error(e.getMessage(),e);
-            formatter.printHelp("Java Exercise 2", options);
-
-            System.exit(1);
-            return;
-        }
-
-        final boolean bye = cmd.hasOption('b');
-
-        if (cmd.hasOption('c')) {
-        	String countStr = cmd.getOptionValue("count");
-      		try {
-				Integer i = Integer.parseInt(countStr);
-				while (i-- > 0) {
-					log.info("Hello World");
-				}
-			} catch (NumberFormatException e) {
-				log.error("Invalid value for count variable");
-				System.exit(1);
-	            return;
-			}
-        }
-        
-        if(bye) {
-        	log.info("Bye world");
-        }
+		GreetingService service = new GreetingService();
+		service.process(args);
 
         log.info("Java Exercise 3: Application ended!");
 	}
